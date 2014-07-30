e429be8cc73b1b060c9a9d67d6e2578280e902d0
radman404 <kylefleming@gmail.com>
2014-07-30 23:57:13 +0100

    Installs needed modules for WAMN script.
    Installs needed modules for WAMN script. Tested on Linux Mint.

---
+++ Install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#################################
+#	Install script for wamn.py	#
+#################################
+if [[ $EUID -ne "0" ]]; then
+	echo "Needs to be run as root"
+	exit 
+else
+	mkdir /tmp/wamnsetup && cd /tmp/wamnsetup
+	wget https://pygmaps.googlecode.com/files/pygmaps-0.1.1.tar.gz
+	wget https://bootstrap.pypa.io/get-pip.py
+	python get-pip.py
+	gunzip pygmaps-0.1.1.tar.gz && tar -xvf pygmaps-0.1.1.tar
+	cd pygmaps-0.1.1/
+	python setup.py install
+	cd ../
+	pip install pygeoip pylogsparser mechanize pytz
+	cd ~/
+	rm -rf /tmp/wamnsetup
+fi
