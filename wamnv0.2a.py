ab5127a04ff3f4dae77d4b16a0ce346ab1a5008e
radman404 <kylefleming@gmail.com>
2014-08-02 02:40:17 +0100

    Added in some features
    Asks for path to log, supports gzip compressed logfiles, also finds
    users IP and geolocates them and builds the gmap to their coordinates,
    fixed a mistake in the private IP checker thanks to Terry IP for
    pointing that out and for bringing up the compressed log support issue.
    I thought this was enough change to illicit a change in version and up
    it to 0.2a.

---
+++ wamnv0.2a.py
@@ -0,0 +1,231 @@
+import sys, signal, socket
+import urllib2
+import urlparse 
+import re 
+import pygmaps
+import pygeoip 
+import mechanize
+import os
+import gzip
+from logsparser.lognormalizer import LogNormalizer as LN
+from time import sleep
+os.system('clear')
+normalizer = LN('/usr/local/share/logsparser/normalizers')
+gi = pygeoip.GeoIP('./GeoLiteCity.dat')
+countries = {}
+latlong = {}
+
+def geo_menu():
+  print 'Geolocation Menu'
+  print '1) Enter and locate an IP Addr'
+  print '2) Enter a url, resolve IP and geolocate'
+  print '3) Log Check and IP geolocate (sshd)'
+  print '0) Exit'
+  geo_option = raw_input('Please choose an option: ')
+  try:
+    choice = geomenudict[geo_option]
+    choice()
+  except KeyError:
+    print "try again..."
+    geo_menu()
+ 
+def logcheck():
+  print "This function can also handle gzip compressed logfiles"
+  LOG =  raw_input('Enter the path to the log file: ')
+  if LOG.endswith('.gz'):
+    auth_logs = gzip.GzipFile(LOG, 'r')
+  else : 
+    auth_logs = open(LOG, 'r')
+  
+  attacks = {}
+  users = {}
+  origin_unknown = {}
+  print "Parsing log file"
+  for log in auth_logs:
+    l = {"raw": log }
+    normalizer.normalize(l)
+    if l.get('action') == 'fail' and l.get('program') == 'sshd':	
+      u = l['user']
+      p = l['source_ip']
+      oct1, oct2, oct3, oct4 = [int(i) for i in p.split('.')] #split IP so we can check for private addr
+      if oct1 == 192 and oct2 == 168 or oct1 == 172 and oct2 in range(15, 32) or oct1 == 10: #check for private addr
+        print "Private ip attack, %s No geolocation available" % str(p)
+      if attacks.has_key(p): #if not private do geolocate
+        attacks[p] = attacks.get(p, 0)+ 1
+        countryRecord(p)
+      else:
+        printRecord(p)
+        attacks[p] = attacks.get(p, 0)+ 1
+      users[u] = users.get(u, 0)+ 1
+  sort(attacks, users)
+  gmaps()
+  en2con()
+  main()
+
+# reads takes ip addr as string. Reads .dat file, prints coresponding lat, long and area of ip
+def geo_ip():
+  print "Enter IP address"
+  ipaddr = raw_input("Please enter an IP Address: ")
+  printRecord(ipaddr) 
+  gmaps()
+
+def countryRecord(p):
+  # This function gets run just to keep the country attack count right if the Ip address doesn't need geolocated again.
+  # This needs fixed (As in removed, there must be a nicer way to do it!)
+  rec = gi.record_by_name(p)
+  try:
+    country = rec['country_name']
+    if country:
+      countries[rec['country_name']] = countries.get(rec['country_name'], 0)+ 1
+  except TypeError:
+    print "No country in DB"
+  return
+
+def printRecord(tgt):
+  rec = gi.record_by_name(tgt)
+  try:
+    city = rec['city']
+    if city:
+      city = city.encode('utf8')
+  except TypeError:
+    print "no city in DB"
+  try:
+    region = rec['time_zone']
+  except TypeError:
+    print "no timezone in db"
+  try:
+    country = rec['country_name']
+    if country:
+      countries[rec['country_name']] = countries.get(rec['country_name'], 0) + 1
+	
+  except TypeError:
+    print "No country in db"
+  try:
+    long = rec['longitude']
+    lat = rec['latitude']
+    if lat and long:
+      latlong[rec['latitude']] = rec['longitude']		
+  except TypeError:
+    print "No coords in db"
+  try:
+    print '[*] Target: ' + tgt + ' Geo-located. '
+    print '[+] ' + str(city) +', '+str(region)+ ', '+ str(country)
+    print '[+] Latitude: '+str(lat)+ ', ' +str(long)
+    print ''
+  except UnboundLocalError:
+    print "error"
+  return
+
+def gmaps():
+  #Build map
+  map = pygmaps.maps(latitude, longitude, 3)
+  for p,k in latlong.items():
+    #Add in locations
+    map.addpoint(float(p), float(k))
+  #Draw the map
+  map.draw('./map.html')
+
+
+def sort(attacks, users):
+  
+  print "Attack IP Sorted in order"	
+  for i,j in sorted(attacks.items(), cmp = lambda a,b: cmp(a[1], b[1]) ):
+    #print "Attack IP sorted in order"
+    print "\t%s (%i attempts)" % (i,j)
+  
+  print "Attacking countries Sorted in order"
+  for p,k in sorted(countries.items(), cmp = lambda c,d: cmp(c[1], d[1]) ):
+   # print "Attacking countries sorted in order"
+    print "\t%s (%i attempts)" % (p,k)
+
+  print "Usernames used in attacks Sorted in Order only if 10 or more attempts made with same username"
+  for u,a in sorted(users.items(), cmp = lambda e,f: cmp(e[1], f[1]) ):
+    if a > 9:
+      print "\t%s (%i attempts)" % (u,a)
+
+def getlocation():
+  i = urllib2.Request("http://icanhazip.com")
+  p = urllib2.urlopen(i)
+  ip = p.read() 
+
+  s = gi.record_by_name(ip)
+  try:
+    global latitude 
+    latitude = s['latitude']
+    global longitude 
+    longitude = s['longitude']
+  except TypeError:
+    print "No coordinates held for your IP address"
+
+def resolvegeoip():
+  ur = raw_input("URL [>] ")
+  if "http://" in ur or "https://" in ur:
+    req = urllib2.Request(ur)
+  else:
+    url = "http://" + ur
+    req = urllib2.Request(url)
+  webdat = urllib2.urlopen(req)
+  ip = socket.gethostbyname(urlparse.urlparse(webdat.geturl()).hostname)
+  print ip
+  printRecord(ip)
+  gmaps()
+  en2con()
+  main()
+
+def ctrlc_catch(signal, frame):
+
+  print "\n Exiting clean"
+  sys.exit(0)
+
+def en2con(Prompt='Hit Enter to continue'):
+	raw_input("Hit Enter to continue")
+	
+def main():
+  getlocation()
+  signal.signal(signal.SIGINT, ctrlc_catch)
+  print 'Welcome to WAMN.' 
+  print '1) Automatic set-up and run on local system' 
+  print '2) Set advanced options' 
+  print '3) Perform specific task from WAMN toolkit.' 
+  print '0) Exit'
+  option = raw_input('Please choose an option: ') 
+  try:
+    main_choice = main_menu[option]
+    main_choice()
+  except KeyError:
+    print "Invalid selection"
+    main()
+	
+def toolsmenu(): 
+  print 'Tools Main Menu'
+  print '1) Geolocation Tool'
+  print '2) TBC'
+  print '3) TBC'
+  print '0) Exit'
+  tools_option = raw_input('Please choose an option: ')
+  try:
+    choice = tools_dict[tools_option]
+    choice()
+  except KeyError:
+    print "Invalid selection"
+    toolsmenu()
+	
+main_menu = {
+  '2': geo_ip,
+# '2': tbc1,
+  '3': toolsmenu,
+  '0': exit
+  }
+tools_dict = {
+  '1': geo_menu,
+# '2': tbc,
+# '3': tbc,
+  '0': exit
+  }
+geomenudict = {
+  '1': geo_ip,
+  '2': resolvegeoip,
+  '3': logcheck,
+  '0': exit
+  }
+main()
