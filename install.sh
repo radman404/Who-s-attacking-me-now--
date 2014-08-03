d18218d5d35ddf338630688f33e51012875c3e96
Paul Mason <pdm126@hotmail.co.uk>
2014-08-03 01:38:07 +0100

    Update and rename Install.sh to install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ if [[ $EUID -ne "0" ]]; then
 	echo "Needs to be run as root"
 	exit 
 else
-	mkdir /tmp/wamnsetup && cd /tmp/wamnsetup
+	wget -N http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+        gunzip GeoLiteCity.dat.gz
+        mkdir /tmp/wamnsetup && cd /tmp/wamnsetup
 	wget https://pygmaps.googlecode.com/files/pygmaps-0.1.1.tar.gz
 	wget https://bootstrap.pypa.io/get-pip.py
 	python get-pip.py
